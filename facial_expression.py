ccd9116e87ea477230de52762b99e39a4da1555c
artursahak <58938408+artursahak@users.noreply.github.com>
2020-04-24 11:53:59 +0400

    Add files via upload

---
+++ facial_expression.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+import dlib
+
+cap = cv2.VideoCapture(0)
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("C:\wheels\shape_predictor_68_face_landmarks.dat")
+
+# font 
+font = cv2.FONT_HERSHEY_SIMPLEX 
+  
+# org 
+org = (50, 50) 
+  
+# fontScale 
+fontScale = 1
+   
+# Blue color in BGR 
+color = (255, 0, 0) 
+  
+# Line thickness of 2 px 
+thickness = 2
+   
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray)
+    for face in faces:
+        x1 = face.left()
+        y1 = face.top()
+        x2 = face.right()
+        y2 = face.bottom()
+        #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+        landmarks = predictor(gray, face)
+
+        for n in range(0, 68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            cv2.circle(frame, (x, y), 2, (255, 0, 0), -1)
+            cv2.putText(frame, "{}".format(n), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 255, 0), 1, lineType=cv2.LINE_AA)
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+    
+  
